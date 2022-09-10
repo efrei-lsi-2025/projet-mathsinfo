6b7d64ae0cc2b70e3439510f5c4fc9c81d95739c
JiveOff <antoine@jiveoff.fr>
2022-09-10 17:46:12 +0200

    Canvas avec superposition

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import fs from 'fs';
-import { Interstation, Station } from './types';
+import fs from "fs";
+import { Interstation, Station } from "./types";
+import { getMetroColor } from "./utils";
 
-import canvasLibrary from 'canvas';
+import canvasLibrary from "canvas";
 const canvas = canvasLibrary.createCanvas(987, 952);
-const canvasCtx = canvas.getContext('2d');
+const canvasCtx = canvas.getContext("2d");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
@@ -14,34 +15,57 @@ const interstations: Interstation[] = [];
 
 async function readAndParseData() {
   console.log("Reading metro.txt file.");
-  let lines = await fs.readFileSync('./docs/metro.txt', 'utf8');
-  let linesArray = lines.split('\n').slice(15);
-  for(let i in linesArray) {
+  let lines = await fs.readFileSync("./docs/metro.txt", "utf8");
+  let linesArray = lines.split("\n").slice(15);
+  for (let i in linesArray) {
     let lineSplit = linesArray[i].split(";");
-    if(lineSplit[0].charAt(0) === "V") {
+    if (lineSplit[0].charAt(0) === "V") {
       stations.push({
         num: parseInt(lineSplit[0].slice(2).slice(0, 4)),
         nom: lineSplit[0].slice(6).trim(),
-        ligne: parseInt(lineSplit[1].trim()),
+        ligne: lineSplit[1].trim(),
         terminus: lineSplit[2].split(" ")[0] === "True",
-        branchement: parseInt(lineSplit[2].split(" ")[1])
+        branchement: parseInt(lineSplit[2].split(" ")[1]),
+        adjacentStations: [],
+        adjacentInterstations: [],
       });
     } else {
+
+      let station_1 = stations.find(
+        (s) => s.num === parseInt(linesArray[i].slice(2).split(" ")[0])
+      )
+
+      let station_2 = stations.find(
+        (s) => s.num === parseInt(linesArray[i].slice(2).split(" ")[1])
+      )
+
       interstations.push({
-        station_1: stations.find((s) => s.num === parseInt(linesArray[i].slice(2).split(" ")[0])),
-        station_2: stations.find((s) => s.num === parseInt(linesArray[i].slice(2).split(" ")[1])),
-        temps: parseInt(linesArray[i].split(" ")[2])
+        station_1,
+        station_2,
+        temps: parseInt(linesArray[i].split(" ")[2]),
       });
+
+      if (station_1) {
+        station_1.adjacentStations.push(station_2);
+        station_1.adjacentInterstations.push(interstations[interstations.length - 1]);
+      }
+
+      if (station_2) {
+        station_2.adjacentStations.push(station_1);
+        station_2.adjacentInterstations.push(interstations[interstations.length - 1]);
+      }
     }
   }
 
   // example data from pospoints: 744;811;Bibliothèque@François@Mitterand
-  let positions = await fs.readFileSync('./docs/pospoints.txt', 'utf8');
-  let positionsArray = positions.split('\n');
-  for(let i in positionsArray) {
+  let positions = await fs.readFileSync("./docs/pospoints.txt", "utf8");
+  let positionsArray = positions.split("\n");
+  for (let i in positionsArray) {
     let lineSplit = positionsArray[i].split(";");
-    let station = stations.find((s) => s.nom === lineSplit[2].replace(/@/g, " ").trim());
-    if(station) {
+    let station = stations.find(
+      (s) => s.nom === lineSplit[2].replace(/@/g, " ").trim()
+    );
+    if (station) {
       station.lat = parseInt(lineSplit[0]);
       station.lng = parseInt(lineSplit[1]);
     }
@@ -49,23 +73,35 @@ async function readAndParseData() {
 }
 
 async function startCanvasAndServer() {
-  const image = await canvasLibrary.loadImage('./docs/metrof_r.png');
+  const image = await canvasLibrary.loadImage("./docs/metrof_r.png");
+  canvasCtx.globalAlpha = 0.4;
   canvasCtx.drawImage(image, 0, 0, 987, 952);
+  canvasCtx.globalAlpha = 1;
 
   // place point for each station on the map
-  for(let i in stations) {
+  for (let i in stations) {
     canvasCtx.beginPath();
     canvasCtx.arc(stations[i].lat, stations[i].lng, 3, 0, 2 * Math.PI);
-    canvasCtx.fillStyle = "red";
+    canvasCtx.fillStyle = getMetroColor(stations[i].ligne);
     canvasCtx.fill();
+  }
+
+  // draw line between stations
+  for (let i in interstations) {
+    canvasCtx.beginPath();
+    if (interstations[i].station_1 && interstations[i].station_2) {
+      canvasCtx.moveTo(interstations[i].station_1.lat, interstations[i].station_1.lng);
+      canvasCtx.lineTo(interstations[i].station_2.lat, interstations[i].station_2.lng);
+      canvasCtx.strokeStyle = getMetroColor(interstations[i].station_1?.ligne);
+      canvasCtx.stroke();
+    }
   }
 }
 
 async function main() {
-
   await readAndParseData();
-  console.log("Done.")
-  
+  console.log("Done.");
+
   let station_1 = stations.find((s) => s.nom === "Esplanade de la Défense")!;
   let station_2 = stations.find((s) => s.nom === "Porte de la Villette")!;
 
@@ -74,31 +110,28 @@ async function main() {
   // express listen
   const router = express.Router();
 
-  router.get('/', (req: any, res: any) => {
-    res.send('Hello World!');
+  router.get("/", (req: any, res: any) => {
+    res.send("Hello World!");
   });
 
-  router.get('/canvas', (req: any, res: any) => {
-    res.set('Content-Type', 'image/png');
+  router.get("/canvas", (req: any, res: any) => {
+    res.set("Content-Type", "image/png");
     canvas.createPNGStream().pipe(res);
   });
 
-  router.get('/stations', (req: any, res: any) => {
+  router.get("/stations", (req: any, res: any) => {
     res.send(stations);
   });
 
-  router.get('/interstations', (req: any, res: any) => {
+  router.get("/interstations", (req: any, res: any) => {
     res.send(interstations);
   });
 
-  app.use('/', router);
+  app.use("/", router);
 
   app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
   });
-
 }
-
-
 
 main();
--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,17 @@
 export type Station = {
   num: number;
   nom: string;
-  ligne: number;
+  ligne: string;
   terminus: boolean;
   branchement: number;
   lat?: number;
   lng?: number;
+  adjacentStations: Station[];
+  adjacentInterstations: Interstation[];
 };
 
 export type Interstation = {
-  station_1: Station | undefined;
-  station_2: Station | undefined;
+  station_1?: Station;
+  station_2?: Station;
   temps: number;
 }
